cc42d23be1baf3ea174e3287ab1c3576c4eae476
NgocTu <dhhtuss512@gmail.com>
2017-05-30 19:53:37 +0700

    show account information 2

---
+++ src/main/java/test/Account.java
@@ -0,0 +1,36 @@
+package test;
+
+/**
+ * Created by Admin on 05/30/17. fix
+ */
+public class Account {
+    private long id;
+    private String ownerName;
+    private double blance;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public double getBlance() {
+        return blance;
+    }
+
+    public void setBlance(double blance) {
+        this.blance = blance;
+    }
+
+
+}
